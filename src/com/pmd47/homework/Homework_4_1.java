c9ef1f080ab2a0caebdaf6537aef5344c022c5a0
6kamens <slil.puangpoom@gmail.com>
2021-03-10 01:50:50 +0700

    hw4.1

--- src/com/pmd47/homework/Homework_4_1.java
+++ src/com/pmd47/homework/Homework_4_1.java
@@ -1,5 +1,28 @@
 package com.pmd47.homework;
 
 public class Homework_4_1 {
+    public static void main(String[] args) {
+        
+        //1
+        int score = 55 ;
+        //a
+        if(score >= 80) System.out.println("Good");
+        //b
+        if(score >= 50) System.out.println("normal");
+        //c
+        if(score < 50) System.out.println("fail");
 
+        //2
+        boolean isHandSome = true;
+        //a
+        if(score >= 80 || isHandSome){
+            System.out.println("Good");
+        }else if(score >= 50){
+            System.out.println("normal");
+        }else if(score < 50){
+            System.out.println("fail");
+        }
+
+
+    }
 }
