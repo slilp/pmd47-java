94d57148ad1049e65c11fe703b507d82d1d9a1c9
6kamens <slil.puangpoom@gmail.com>
2021-03-10 02:15:45 +0700

    hw5

--- src/com/pmd47/homework/Homework_4_5.java
+++ src/com/pmd47/homework/Homework_4_5.java
@@ -1,5 +1,23 @@
 package com.pmd47.homework;
 
 public class Homework_4_5 {
+    public static void main(String[] args) {
 
+        //Lab9
+        for(int count = 1 ; count <= 20 ; count++){
+            if(count == 11){
+                System.out.println(count);
+                continue;
+            } 
+        }
+
+        //Lab10
+        for(int count = 1 ; count <= 10 ; count++){
+            if(count == 2){
+                continue;
+            } 
+            System.out.println(count);
+        }
+
+    }
 }
--- src/com/pmd47/homework/Homework_5.java
+++ src/com/pmd47/homework/Homework_5.java
@@ -1,5 +1,22 @@
 package com.pmd47.homework;
 
 public class Homework_5 {
+    public static void main(String[] args) {
+        
+        String word = "HELLO";
+
+        iamNotReturn(word);
+
+        System.out.println(iamReturn(word));
+
+    }
+
+    public static void iamNotReturn(String text){
+        System.out.println(text);
+    }
+
+    public static String iamReturn(String text){
+        return text;
+    }
 
 }
