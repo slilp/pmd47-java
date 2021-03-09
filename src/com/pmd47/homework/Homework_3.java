fa075bf67508e541636c2b183180d19b280edf95
6kamens <slil.puangpoom@gmail.com>
2021-03-10 01:45:44 +0700

    hw3

--- src/com/pmd47/homework/Homework_3.java
+++ src/com/pmd47/homework/Homework_3.java
@@ -1,5 +1,39 @@
 package com.pmd47.homework;
 
+
 public class Homework_3 {
 
+    public static void main(String[] args) {
+
+        // 1
+        int i = 20;
+        // a
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
+        System.out.println(i);
+        i = 20 ;
+        // b
+        --i;
+        --i;
+        --i;
+        --i;
+        --i;
+        System.out.println(i);
+
+        //2
+        float f1 = 5.5f;
+        float f2 = 5f;
+        if((f1 == 5.5f) && (f2 == 5f)) System.out.println(true);
+
+        //3
+        char a = 'A';
+        char b = 'B';
+
+        if((a == 'A') || (b == 'C')) System.out.println(true);
+
+
+    }
 }
