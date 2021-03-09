13bf83e89cb92c1a26c5b23cbdf496687562f9e8
6kamens <slil.puangpoom@gmail.com>
2021-03-10 01:54:27 +0700

    hw4.3

--- src/com/pmd47/homework/Homework_4_3.java
+++ src/com/pmd47/homework/Homework_4_3.java
@@ -1,5 +1,22 @@
 package com.pmd47.homework;
 
 public class Homework_4_3 {
+    public static void main(String[] args) {
+        
+        double score = 53;
 
+        if(score >= 80){
+            System.out.println("A");
+        }else if(score >= 70){
+            System.out.println("B");
+        }else if(score >= 60){
+            System.out.println("C");
+        }else if(score >= 50){
+            System.out.println("D");
+        }else if(score >= 40){
+            System.out.println("F");
+        }else{
+            System.out.println("E");
+        }
+    }
 }
