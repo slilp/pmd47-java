191bb2d4d8a48019a5ed48c80ce9ae5a889e7803
6kamens <slil.puangpoom@gmail.com>
2021-03-12 01:37:21 +0700

    hw6

---
+++ src/com/pmd47/homework/Homework_6.java
@@ -0,0 +1,10 @@
+package com.pmd47.homework;
+
+public class Homework_6 {
+    public static void main(String[] args) {
+         String test = Character.toString('t') + Character.toString('e') + Character.toString('s') + Character.toString('t');
+
+         System.out.println(test);
+         
+    }
+}
